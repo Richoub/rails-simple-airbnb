3ac844e0ad6a1796ac9372477b93e4e037feffad
Richard Bates <richjbates@gmail.com>
2020-12-10 21:07:03 +0100

    aribnb skeleton

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning up database"
+Flat.destroy_all
+puts "database is clean"
+
+puts "adding to the database"
+10.times do
+  flat = Flat.create!(
+    name: Faker::Marketing.buzzwords,
+    address: Faker::Address.street_address,
+    description: Faker::Marketing.buzzwords,
+    price_per_night: Faker::Number.number(digits: 3),
+    number_of_guests: Faker::Number.number(digits: 1)
+  )
+  puts "Flat #{flat.id} has been created"
+end
+puts "done"
